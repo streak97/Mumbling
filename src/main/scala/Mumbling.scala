5ed625f56708e1e0636b142f2e4a1a14f5ed4311
Jakob Overrein <jakoboverrein@hotmail.com>
2021-05-28 14:31:21 +0200

    Added check for only letters

--- src/main/scala/Mumbling.scala
+++ src/main/scala/Mumbling.scala
@@ -1,5 +1,7 @@
 object Mumbling extends App {
     def accum(input: String): String = {
+        if (!isAlpha(input)) return "Not a valid input"
+
         val lowercaseInput = input.toLowerCase()
         val arrInput: Array[String] = lowercaseInput.sliding(1).toArray
 
@@ -9,5 +11,15 @@ object Mumbling extends App {
         }
 
         arrInput.mkString("-")
+    }
+
+    def isAlpha(input: String): Boolean = {
+        val chars: Array[Char] = input.toCharArray
+
+        for (c <- chars) {
+            if (!c.isLetter) return false
+        }
+
+        true
     }
 }
--- src/test/scala/MumblingTest.scala
+++ src/test/scala/MumblingTest.scala
@@ -9,4 +9,10 @@ class MumblingTest extends FunSuite {
         assert(Mumbling.accum("RqaEzty") === "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy")
         assert(Mumbling.accum("cwAt") === "C-Ww-Aaa-Tttt")
     }
+
+    test("With numbers") {
+        assert(Mumbling.accum("4bcd") === "Not a valid input")
+        assert(Mumbling.accum("AbCd3") === "Not a valid input")
+        assert(Mumbling.accum("Ab5d3") === "Not a valid input")
+    }
 }
